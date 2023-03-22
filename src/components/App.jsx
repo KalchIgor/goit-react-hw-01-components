ab1538e5462d6c6f45f989e5ac20dae6c35b094f
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-03-22 12:39:06 +0200

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Profile } from './Profile/Profile';
+import { Statistics} from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
 
 import user from './Profile/user.json';
@@ -14,7 +16,8 @@ export const App = () => {
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
-/>
+      />
+   <Statistics title="Upload stats" stats={data} />
 
 </>
   );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,1 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+const createColor = () => {
+  const color =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    0.5 +
+    ')';
+
+  return color;
+};
+
+export const Statistics = ({ title, stats }) => {
+    return (<section className='{css.title}'>
+
+        {title && <h2 className={css.title}> {title} </h2>}
+ <ul className={css.statList}>
+    {stats.map(stat => (
+        <li className={css.item} key={stat.id} style={{ backgroundColor: createColor() }}>
+        <span className={css.label}>{stat.label}</span>
+        <span className={css.percentage}>{stat.percentage}%</span>
+      </li>
+    ))}    
+  </ul>
+
+    </section>)
+}
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+      }),
+    ),
+  };
