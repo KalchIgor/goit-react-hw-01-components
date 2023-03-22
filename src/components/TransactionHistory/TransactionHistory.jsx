92951af6291328e24624144dba293e1904a7232b
Игорь Кальчин <108684879+KalchIgor@users.noreply.github.com>
2023-03-22 17:16:38 +0200

    TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,1 +1,46 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 
+const createColor = () => {
+  const color =
+    'rgba(' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    Math.round(Math.random() * 255) +
+    ',' +
+    0.5 +
+    ')';
+
+  return color;
+};
+
+export const TransactionHistory = ({ items }) => (
+<table className={css.transactionHistory}>
+  <thead>
+    <tr style={{ backgroundColor: createColor() }}>
+      <th>Type</th>
+      <th>Amount</th>
+      <th>Currency</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {items.map(item => (
+        <tr style={{ backgroundColor: createColor() }} key={item.id}>
+        <td>{item.type}</td>
+        <td>{item.amount}</td>
+        <td>{item.currency}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+)
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
